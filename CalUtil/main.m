edfdb875b2893efb5137bd909afbcc21fd9abde8
Roger Herikstad <roger.herikstad@gmail.com>
2012-01-15 12:22:33 +0800

    Initial commit of working version. Allows for the creation of iCal events from the command line using a simple syntax

--- CalUtil/main.m
+++ CalUtil/main.m
@@ -7,15 +7,56 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CalendarStore/CalendarStore.h>
 
 int main (int argc, const char * argv[])
 {
 
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
-
+    //parse the input parameters
+    //user userDefaults for that
+    NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
+    NSString *calName = [args stringForKey:@"calName"];
+    //get the defaults calendar
+    NSArray *calendars = [[CalCalendarStore defaultCalendarStore] calendars];
+    //check if the requested calendar exists
+    NSArray *matches = [calendars filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"title==%@",calName]];
+    //did we find one
+    if([matches count]==0)
+    {
+        printf("No calendar matching %s found. Exiting...\n",[calName UTF8String]);
+        [pool drain];
+        return 0;
+    }
+    //select the first match
+    CalCalendar *cal = [matches objectAtIndex:0];
+    //get the date
+    NSDateFormatter *dateFormat = [[[NSDateFormatter alloc] init] autorelease];
+    [dateFormat setDateFormat: @"dd/MM/yyyy 'at' hh:mm"];
+    NSString *sdate = [args stringForKey:@"startDate"];
+    //NSLog(@"Start date: %@",[dateFormat stringFromDate: [dateFormat dateFromString:sdate]]);
+    NSString *edate = [args stringForKey:@"endDate"];
+    //NSLog(@"End date: %@",edate);
+    //get the title
+    NSString *stitle = [args stringForKey:@"title"];
+    NSString *loc = [args stringForKey:@"location"];
+    //create the event
+    CalEvent *event = [CalEvent event];
+    event.calendar = cal;
+    event.title = stitle;
+    event.startDate = [dateFormat dateFromString:sdate];
+    event.endDate = [dateFormat dateFromString:edate];
+    event.location = loc;
+    //save the event
+    NSError *calError;
+    if ([[CalCalendarStore defaultCalendarStore] saveEvent:event span:CalSpanThisEvent error:&calError] == NO)
+    {
+        printf("Could not save event\n");
+    }
+    else
+    {
+        printf("Event created\n");
+    }
     [pool drain];
     return 0;
 }
