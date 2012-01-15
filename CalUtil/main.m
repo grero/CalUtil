64664142314a1de494659bf5696b471191c1ba36
Roger Herikstad <roger.herikstad@gmail.com>
2012-01-15 11:13:46 +0800

    Initial Commit

---
+++ CalUtil/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CalUtil
+//
+//  Created by Roger Herikstad on 15/1/12.
+//  Copyright 2012 NUS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
